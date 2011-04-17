1b49993467dc8f5400c57208a10febc55035352c
arvida <arvid@winstondesign.se>
2011-04-17 23:08:27 +0200

    Made path helpers into class methods

--- lib/ya_feedr/config.rb
+++ lib/ya_feedr/config.rb
@@ -1,18 +1,22 @@
 module YaFeedr
   class Config
     attr_accessor :title, :about, :feeds, :authentication
-    # Path to gems lib/ya_feedr dir
-    def root
-      @root ||= File.expand_path( File.dirname(__FILE__) )
-    end
-
-    # Path dir where the app is run from
-    def app_root
-      @app_root ||= Dir.pwd
-    end
 
     def file_path(path)
-      File.exists?(File.join(YaFeedr.config.app_root, path)) ? File.join(YaFeedr.config.app_root, path) : File.join(YaFeedr.config.root, path)
+      File.exists?(File.join(YaFeedr::Config.app_root, path)) ? File.join(YaFeedr::Config.app_root, path) : File.join(YaFeedr::Config.root, path)
     end
+
+    class << self
+      # Path to gems lib/ya_feedr dir
+      def root
+        @root ||= File.expand_path( File.dirname(__FILE__) )
+      end
+
+      # Path dir where the app is run from
+      def app_root
+        @app_root ||= Dir.pwd
+      end
+    end
+
   end
 end
